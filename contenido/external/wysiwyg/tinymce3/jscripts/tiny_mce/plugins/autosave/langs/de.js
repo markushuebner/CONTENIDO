a2ff55a620ccc3b42a36c54e2a555e8dd0c8aa37
Murat Purc <murat@purc.de>
2019-02-11 21:36:54 +0100

    CON-2786: Provide missing German tranlations for TinyMCE 3 plugins.

---
+++ contenido/external/wysiwyg/tinymce3/jscripts/tiny_mce/plugins/autosave/langs/de.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('de.autosave',{
+restore_content: "Automatisch gespeicherten Inhalt wiederherstellen",
+warning_message: "Wenn Sie den automatisch gespeicherten Inhalt wiederherstellen, verlieren Sie den gesamten Inhalt, das gerade im Editor ist.\n\nSind Sie sich sicher, dass Sie den automatisch gespeicherten Inhalt wiederherstellen wollen?"
+});
